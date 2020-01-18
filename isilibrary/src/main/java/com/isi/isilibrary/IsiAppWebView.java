1e6769ce45fd5e711b2e280336c905ef4b751f64
edoardogullo@libero.it <ludo221000>
2020-01-18 14:35:06 +0100

    WebView

---
+++ isilibrary/src/main/java/com/isi/isilibrary/IsiAppWebView.java
@@ -0,0 +1,35 @@
+package com.isi.isilibrary;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.webkit.WebView;
+
+public class IsiAppWebView extends WebView {
+
+    public IsiAppWebView(Context context) {
+        super(context);
+    }
+
+    public IsiAppWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public IsiAppWebView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public IsiAppWebView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev){
+        super.dispatchTouchEvent(ev);
+
+        ((Activity)getContext()).onTouchEvent(ev);
+
+        return true;
+    }
+}
